67bad3fc78023fa1671f2de2dfd639b23069179a
Kagisho Mogapi <mogapikagisho@gmail.com>
2023-10-16 13:15:03 +0200

    Add project files.

---
+++ Models/OrderDetail.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantAPI.Model
+{
+    public class OrderDetail
+    {
+        [Key]
+        public int OrderDetailId { get; set; }
+        public long OrderMasterId { get; set; }
+        public int FoodItemId { get; set; }
+        public FoodItem FoodItem { get; set; }
+        public decimal FoodItemPrice { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
---
+++ Models/OrderMaster.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestaurantAPI.Model
+{
+    public class OrderMaster
+    {
+        [Key]
+        public long OrderMasterId { get; set; }
+        [Column(TypeName = "nvarchar(75)")]
+        public string OrderNumber { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string PMethod { get; set; }
+        public decimal GTotal { get; set; }
+        public List<OrderDetail> orderDetails { get; set; }
+
+    }
+}
